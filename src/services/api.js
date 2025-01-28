1ad3a00ff611c7f9320bf7e8dc4a4733e90c2813
JRogerioSC <jo-rogerio@hotmail.com>
2025-01-27 21:23:19 -0300

    novo2

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const api = axios.create({
-    baseURL:'https://conceitos-node-crud.vercel.app/'
+    baseURL:"https://conceitos-node-crud.vercel.app/"
 })
 
 export default api
