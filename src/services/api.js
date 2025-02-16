83c94c66adea0df30b5c1423a3de88b89cce8123
JRogerioSC <jo-rogerio@hotmail.com>
2025-02-16 02:07:38 -0300

    finalment

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 const api = axios.create({
 
   
-    baseURL: "https://conceitos-node-crud.onrender.com"
+    baseURL: "https://conceitos-node-crud-ehf3.onrender.com"
 
    
 })
